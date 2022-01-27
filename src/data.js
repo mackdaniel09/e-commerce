256e598cd5644125e870b6456bf29b3aeb99eed0
mackdaniel09 <mackdaniel06@gmail.com>
2022-01-27 08:26:03 +0800

    first commit

---
+++ src/data.js
@@ -0,0 +1,75 @@
+import FirstImage from "./images/1-removebg-preview.png";
+import SecondImage from "./images/2-removebg-preview.png";
+import ThirdImage from "./images/3-removebg-preview.png";
+export const sliderItems = [
+  {
+    id: 1,
+    // img: "https://i.ibb.co/XsdmR2c/1.png",
+    img: FirstImage,
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FALT 30% OFF FOR NEW ARRIVALS",
+    bg: "fsfafd",
+  },
+  {
+    id: 2,
+    // img: "https://i.ibb.co/XsdmR2c/2.png",
+    img: SecondImage,
+    title: "AUTUMN SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FALT 30% OFF FOR NEW ARRIVALS",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    // img: "https://i.ibb.co/XsdmR2c/3.png",
+    img: ThirdImage,
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FALT 30% OFF FOR NEW ARRIVALS",
+    bg: "fbf0f4",
+  },
+];
+export const categories = [
+  {
+    id: 1,
+    // img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpg",
+    img: "https://source.unsplash.com/random/?shirt",
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    // img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpg",
+    img: "https://source.unsplash.com/random/?loungewear",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 3,
+    // img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpg",
+    img: "https://source.unsplash.com/random/?jacket",
+    title: "LIGHT JACKETS",
+  },
+];
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://source.unsplash.com/random/?jacket",
+  },
+  {
+    id: 2,
+    img: "https://source.unsplash.com/random/?shirt",
+  },
+  {
+    id: 3,
+    img: "https://source.unsplash.com/random/?cap",
+  },
+  {
+    id: 4,
+    img: "https://source.unsplash.com/random/?pants",
+  },
+  {
+    id: 5,
+    img: "https://source.unsplash.com/random/?loungewear",
+  },
+  // {
+  //   id: 6,
+  //   img: "https://source.unsplash.com/random/?shirt",
+  // },
+];
